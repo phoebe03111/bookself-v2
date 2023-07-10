f673d6f8891ec2e9244754d0a9aed1c23a5c8864
Phoebe Chang <phoebe03111@gmail.com>
2023-07-10 16:34:39 -0700

    Set static folder

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,20 @@ app.use(cookieParser());
 app.use("/api/books", bookRoutes);
 app.use("/api/users", userRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  // Any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
